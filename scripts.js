d1092f3fce741922d951cc87ab91535b07750ddf
Dmitry <DmiShisterov@gmail.com>
2020-07-14 19:36:11 +0500

    nnnn

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function eventListing(params) {
         })
     });
 
-    var requestOptions = {
+    /*var requestOptions = {
         method: 'GET',
         redirect: 'follow',
         mode: 'no-cors'
@@ -22,7 +22,25 @@ function eventListing(params) {
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
+*/
+    let url = 'https://github.com/DmitriyShisterov/xsolla-frontend-school-2020/raw/master/src/events.json';
+    // 1. Создаём новый объект XMLHttpRequest
+    var xhr = new XMLHttpRequest();
 
+    // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+    xhr.open('GET', url, false);
+
+    // 3. Отсылаем запрос
+    xhr.send();
+
+    // 4. Если код ответа сервера не 200, то это ошибка
+    if (xhr.status != 200) {
+        // обработать ошибку
+        alert(xhr.status + ': ' + xhr.statusText); // пример вывода: 404: Not Found
+    } else {
+        // вывести результат
+        alert(xhr.responseText); // responseText -- текст ответа.
+    }
 }
 eventListing();
 
