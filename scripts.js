78e3d393a91bf2a4b0e68c4b78e12a6e359769d2
Dmitry <DmiShisterov@gmail.com>
2020-07-16 00:37:21 +0500

    add change city and month

--- scripts.js
+++ scripts.js
@@ -1,15 +1,4 @@
 function eventListing(params) {
-    let bookmark = document.querySelectorAll('.bookmark');
-    bookmark.forEach(function (item, i, bookmark) {
-        item.addEventListener('click', function () {
-            if (!item.classList.contains('checked')) {
-                item.classList.toggle('checked')
-            } else {
-                item.classList.toggle('checked', false);
-            }
-        })
-    });
-
     let eventList = {};
     let requestOptions = {
         method: 'GET',
@@ -23,68 +12,79 @@ function eventListing(params) {
         .then(function (data) {
             eventList = data;
             render(eventList);
-            console.log(eventList);
         })
         .catch(function () {
             //error
         });
-    let el = document.querySelectorAll('select');
-    el.forEach(function (item, i) {
-        item.addEventListener('change', function (e) {
-            if (item.classList.contains('city')) {
-                let cityList = [];
-                let city = item.value;
-                console.log(item.value);
-                eventList.forEach(function (el, j) {
-                    if (el.city === city) {
-                        console.log(el.city)
-                    }
-                })
-                console.log(cityList);
-            }
-            if (item.classList.contains('month')) {
-                console.log('month');
-            }
-        })
-    })
     /*render*/
     function render(x) {
         let eventsWrap = document.querySelector('#eventsWrap');
         for (let i = 0; i < x.length; i++) {
             let name = x[i].name;
-            //let city = x[i].city;
-            let date = x[i].date;
+            let fullDate = x[i].date;
             let img = x[i].image;
 
             let event = document.createElement('div'); // создаем событие
             event.className = 'event';
-            event.style.background = `url${img}`;
-            console.log(event.style.background = `url(${img})`)
+            event.style.backgroundImage = `url(${img})`;
+            console.log()
 
             let dateBookmark = document.createElement('div');// создаем date&bookmark
             dateBookmark.className = 'dateBookmark';
 
             let dates = document.createElement('div');// создаем  date
             dates.className = 'date';
+            date = fullDate.slice(0, 2);
             dates.innerText = date;
 
             let bookmark = document.createElement('div');// создаем bookmark
             bookmark.className = 'bookmark';
-
 
             let eventTitle = document.createElement('div');// создаем eventTitle
             eventTitle.className = 'eventTitle';
             eventTitle.innerText = name;
 
             dateBookmark.append(dates, bookmark);// добавляем к date&bookmark(date,bookmark)
-
             event.append(dateBookmark, eventTitle);// добавляем к event(datebookmark, eventTitle)*/
             eventsWrap.append(event);
-
         }
-
-
     }
+    eventsWrap.onclick = function (event) {
+        let bk = event.target.closest('.bookmark'); // (1)
+        if (!bk) return; // (2)
+        if (!eventsWrap.contains(bk)) return; // (3)
+        if (!bk.classList.contains('checked')) {
+            bk.classList.toggle('checked', true)
+        } else {
+            bk.classList.toggle('checked', false);
+        }
+    };
+    let selects = document.querySelectorAll('select');
+    selects.forEach(function (item, i, selects) {
+        item.addEventListener('change', function (event) {
+            if (item.className === 'city') {
+                let city = item.value;
+                let eventsInCity = []
+                eventList.forEach(function (item, i, eventList) {
+                    if (item.city === city) {
+                        eventsInCity.push(item);
+                    }
+                });
+                console.log(eventsInCity);
+            }
+            if (item.className === 'month') {
+                let month = item.value;
+                let monthEvents = []
+                eventList.forEach(function (item, i, eventList) {
+                    let numberMonth = item.date.slice(3, 5);
+                    if (numberMonth === month) {
+                        monthEvents.push(item);
+                    }
+                });
+                console.log(monthEvents);
+            }
+        })
+    })
 
 }
 eventListing();
